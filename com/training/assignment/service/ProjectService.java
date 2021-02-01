7ea4bea24230558afa01ffbeeaafe5e82c7165d7
NaveenJupally <naveenjkumar768@gmail.com>
2021-02-01 09:38:09 +0530

    initial commit

---
+++ com/training/assignment/service/ProjectService.java
@@ -0,0 +1,28 @@
+package com.training.assignment.service;
+
+import com.training.assignment.framework.Menu;
+import com.training.assignment.framework.MenuItem;
+import com.training.assignment.framework.MenuProvider;
+import com.training.assignment.model.Project;
+
+public class ProjectService {
+
+
+
+    public void createProject(Project project) {
+
+    }
+
+    public void updateProject(String projectId) {
+
+    }
+
+    public void deleteProject(String projectId) {
+
+    }
+
+    public void listProject() {
+
+    }
+
+}
